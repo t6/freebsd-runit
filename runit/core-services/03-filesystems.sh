1166ba5c7c908b45eb548cbfbdd6f76ad4f01103
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-04 04:40:48 +0100

    Automount services are core services
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/03-filesystems.sh
+++ runit/core-services/03-filesystems.sh
@@ -8,3 +8,10 @@ mount -uw / || emergency_shell
 
 msg "Mounting all non-network filesystems..."
 mount -a || emergency_shell
+
+msg "Starting automount daemons..."
+/usr/sbin/automountd ${automountd_flags} || emergency_shell
+/usr/sbin/autounmountd ${autounmountd_flags} || emergency_shell
+
+msg "Enabling autofs..."
+/usr/sbin/automount ${automount_flags} || emergency_shell
