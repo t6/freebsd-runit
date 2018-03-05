0e34da2ba6e5a1a2ae812bc0ae55f38ccd73e0c7
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-05 18:53:21 +0100

    The automount daemons are not core services
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/04-filesystems.sh
+++ runit/core-services/04-filesystems.sh
@@ -17,10 +17,3 @@ mount -a || emergency_shell
 
 msg "Mounting ZFS file systems..."
 zfs mount -a || emergency_shell
-
-msg "Starting automount daemons..."
-/usr/sbin/automountd $(sysrc -qn automountd_flags) || emergency_shell
-/usr/sbin/autounmountd $(sysrc -qn autounmountd_flags) || emergency_shell
-
-msg "Enabling autofs..."
-/usr/sbin/automount $(sysrc -qn automount_flags) || emergency_shell
