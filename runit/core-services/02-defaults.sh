e2a1867f5cac738184e7e7aa06b68406cb2a2944
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-05 04:45:40 +0100

    Don't be cute
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/02-defaults.sh
+++ runit/core-services/02-defaults.sh
@@ -1,36 +1,33 @@
 # Force different defaults than what FreeBSD has.  Can be overwritten
 # again via /etc/sysctl.conf
 
-msg "Setting system defaults..."
+# msg "Setting system defaults..."
+
+# - Let network interfaces accept router advertisments by default
+# - Make FreeBSD installer security options opt-out
+# - Adapted the good bits from https://vez.mrsk.me/freebsd-defaults.txt
+# - Use a better congestion control algorithm than newreno
 kldload cc_cubic > /dev/null 2>&1 || true
-sysctl -f /dev/stdin <<EOF
-# Let network interfaces accept router advertisments by default
-net.inet6.ip6.accept_rtadv=1
+sysctl \
+	net.inet.tcp.cc.algorithm=cubic \
+	net.inet6.ip6.accept_rtadv=1 \
+	security.bsd.see_other_gids=0 \
+	security.bsd.see_other_uids=0 \
+	security.bsd.stack_guard_page=1 \
+	security.bsd.unprivileged_proc_debug=0 \
+	security.bsd.unprivileged_read_msgbuf=0 \
+	kern.randompid=1 \
+	net.inet.icmp.drop_redirect=1 \
+	net.inet.ip.check_interface=1 \
+	net.inet.ip.process_options=0 \
+	net.inet.ip.random_id=1 \
+	net.inet.ip.redirect=0 \
+	net.inet.sctp.blackhole=2 \
+	net.inet.tcp.blackhole=2 \
+	net.inet.tcp.drop_synfin=1 \
+	net.inet.tcp.icmp_may_rst=0 \
+	net.inet.udp.blackhole=1 \
+	security.bsd.hardlink_check_gid=1 \
+	security.bsd.hardlink_check_uid=1
 
-# FreeBSD installer security options
-security.bsd.see_other_gids=0
-security.bsd.see_other_uids=0
-security.bsd.stack_guard_page=1
-security.bsd.unprivileged_proc_debug=0
-security.bsd.unprivileged_read_msgbuf=0
-
-# Some security settings.  Adapted from the good bits of
-# https://vez.mrsk.me/freebsd-defaults.txt
-kern.randompid=1
-net.inet.icmp.drop_redirect=1
-net.inet.ip.check_interface=1
-net.inet.ip.process_options=0
-net.inet.ip.random_id=1
-net.inet.ip.redirect=0
-net.inet.sctp.blackhole=2
-net.inet.tcp.blackhole=2
-net.inet.tcp.drop_synfin=1
-net.inet.tcp.icmp_may_rst=0
-net.inet.udp.blackhole=1
-# XXX: Check if the hardlink_check options really break Poudriere builds.
-security.bsd.hardlink_check_gid=1
-security.bsd.hardlink_check_uid=1
-
-# Use a better congestion control algorithm than newreno 
-net.inet.tcp.cc.algorithm=cubic
-EOF
+# XXX: Check if the hardlink_check options really break Poudriere builds
