06f049f0e572e64f0aa68578f1f802273862fa53
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-05 04:00:07 +0100

    Improve boot messages
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/01-kmods.sh
+++ runit/core-services/01-kmods.sh
@@ -1,7 +1,6 @@
 [ -n "$VIRTUALIZATION" ] && return 0
 
 msg "Loading kernel modules..."
-local _kld
 for _kld in $kld_list ; do
 	load_kld -e ${_kld}.ko $_kld
 done
--- runit/core-services/05-misc.sh
+++ runit/core-services/05-misc.sh
@@ -5,7 +5,7 @@ msg "Setting up loopback interface..."
 ifconfig lo0 inet 127.0.0.1/8 alias
 
 if [ -n "${hostname}" ]; then
-	msg "Setting up hostname to '${hostname}'..."
+	msg "Setting hostname to '${hostname}'..."
 	hostname "${hostname}"
 else
 	msg_warn "Didn't setup a hostname!"
