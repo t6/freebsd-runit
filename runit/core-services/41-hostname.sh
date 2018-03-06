e19db91c19729ffba616b522a708e433cc7dc0c5
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-06 08:00:33 +0100

    Don't set hostname in jails if it's not allowed, also look it up in kenv
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/41-hostname.sh
+++ runit/core-services/41-hostname.sh
@@ -1,7 +1,13 @@
+[ -n "${JAILED}" ] && [ $(sysctl -n security.jail.set_hostname_allowed) -eq 0 ] && return 0
+
 if [ -r "/usr/local/etc/runit/hostname" ]; then
 	_hostname=$(cat /usr/local/etc/runit/hostname)
-	msg "Setting hostname to '${_hostname}'..."
-	hostname "${_hostname}"
+elif [ -n "$(kenv runit.hostname 2> /dev/null)" ]; then
+	_hostname=$(kenv runit.hostname)
 else
 	msg_warn "Didn't setup a hostname!"
+	return 0
 fi
+
+msg "Setting hostname to '${_hostname}'..."
+hostname "${_hostname}"
