5b64db15bb83889d397e09e8baa1db97aea9bcbf
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-06 02:33:58 +0100

    Apply resource limits from /etc/rctl.conf
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/05-misc.sh
+++ runit/core-services/05-misc.sh
@@ -19,3 +19,13 @@ if [ -e "/etc/pf.conf" ]; then
 	msg "Loading PF ruleset from '/etc/pf.conf'..."
 	pfctl -f /etc/pf.conf $(conf pfctl_flags)
 fi
+
+if [ -e "/etc/rctl.conf" ]; then
+	msg "Applying resource limits from '/etc/rctl.conf'..."
+	while read var comments; do
+		case ${var} in
+		\#*|'') ;;
+		*) echo "${var}" ;;
+		esac
+	done < /etc/rctl.conf | xargs rctl -a
+fi
