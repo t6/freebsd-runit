ea8e163c17f6dbf35fc3e28d4853a57b0783a51d
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-07 06:34:13 +0100

    Share ZFS filesystem via `zfs share -a` and allow ZFS mounts in jails
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/31-mount.sh
+++ runit/core-services/31-mount.sh
@@ -1,14 +1,19 @@
-[ -n "${JAILED}" ] && return 0
+if [ -z "${JAILED}" ]; then
+	msg "Mounting / read-write..."
+	mount -uw / || emergency_shell
 
-msg "Mounting / read-write..."
-mount -uw / || emergency_shell
+	msg "Mounting all non-network filesystems..."
+	mount -a || emergency_shell
 
-msg "Mounting all non-network filesystems..."
-mount -a || emergency_shell
+	msg "Mounting ZFS file systems..."
+	zfs mount -va || emergency_shell
 
-msg "Mounting ZFS file systems..."
-zfs mount -va || emergency_shell
-zfs share -a
-if [ ! -r /etc/zfs/exports ]; then
-	touch /etc/zfs/exports
+	msg "Sharing ZFS file systems..."	
+	zfs share -a || emergency_shell
+	if [ ! -r /etc/zfs/exports ]; then
+		touch /etc/zfs/exports
+	fi
+elif [ $(sysctl -n security.jail.mount_allowed) -eq 1 ]; then
+	msg "Mounting ZFS file systems..."
+	zfs mount -a || emergency_shell
 fi
