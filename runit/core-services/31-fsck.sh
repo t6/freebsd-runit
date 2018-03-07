91e600bdec6110aadee337e2c699415f014bda9a
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-07 08:17:40 +0100

    Simplify fsck
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/31-fsck.sh
+++ runit/core-services/31-fsck.sh
@@ -1,56 +1,13 @@
 [ -n "${JAILED}" ] && return 0
 
-stop_boot() {
-	msg_error "ABORTING BOOT"
-	exit 1
-}
-
 if [ ! -r /etc/fstab ]; then
 	msg_warn "No /etc/fstab: skipping disk checks."
 else
-	# During fsck ignore SIGQUIT
-	trap : 3
-
 	msg "Checking filesystems..."
 	fsck -F -p
-
 	err=$?
-	if [ ${err} -eq 3 ]; then
-		# TODO: msg_warn "Some of the devices might not be available; retrying"
-		#root_hold_wait
-		sleep 10
-		msg "Restarting file system checks..."
-		fsck -F -p
-		err=$?
+	if [ ${err} -neq 0 ]; then
+		msg_error "fsck exited with status ${err}"
+		emergency_shell
 	fi
-
-	case ${err} in
-		0)
-		;;
-		2)
-			stop_boot
-			;;
-		4)
-			msg_warn "Rebooting..."
-			# reboot
-			# echo "Reboot failed; help!"
-			# stop_boot
-			exit 1
-			;;
-		8)
-			msg_error "Automatic file system check failed; help!"
-			stop_boot
-			;;
-		12)
-			msg_error "Boot interrupted."
-			stop_boot
-			;;
-		130)
-			stop_boot
-			;;
-		*)
-			msg_error "Unknown error ${err}; help!"
-			stop_boot
-			;;
-	esac
 fi
