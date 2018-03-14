249340a2061cd737bd631bbf6baddad38b3e1183
Tobias Kortkamp <tobik@FreeBSD.org>
2018-03-14 07:11:57 +0100

    Add core service to update CPU microcode
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

---
+++ runit/core-services/33-microcode_update.sh
@@ -0,0 +1,21 @@
+[ -n "${JAILED}" ] && return 0
+msg "Updating CPU microcode..."
+
+if ! kldstat -q -m cpuctl; then
+	if ! kldload cpuctl > /dev/null 2>&1; then
+		msg_error "Can't load cpuctl module"
+		emergency_shell
+	fi
+fi
+
+for i in $(jot $(sysctl -n hw.ncpu) 0); do
+	cpucontrol -u -d /usr/local/share/cpucontrol \
+		/dev/cpuctl${i} 2>&1 \
+		| logger -p daemon.notice -t microcode_update || \
+		(msg_error "Microcode update failed" && emergency_shell)
+done
+cpucontrol -e /dev/cpuctl0 >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	msg_error "Re-evalulation of CPU flags failed"
+	emergency_shell
+fi
