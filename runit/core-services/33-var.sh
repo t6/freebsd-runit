e902399db2117af6eed068756597d4e773f95865
Tobias Kortkamp <tobik@FreeBSD.org>
2018-04-10 10:32:40 +0200

    Mount tmpfs on /var/run and make it transient

--- runit/core-services/33-var.sh
+++ runit/core-services/33-var.sh
@@ -1,10 +1,8 @@
 msg "Populating /var directory..."
 
-if [ -d /var/run ] && [ -d /var/db ] && [ -d /var/empty ]; then
-	true
-else
-	/usr/sbin/mtree -deiU -f /etc/mtree/BSD.var.dist -p /var > /dev/null
-fi
+mkdir -p /var/run
+mount -t tmpfs -o size=8m tmpfs /var/run
+/usr/sbin/mtree -deiU -f /etc/mtree/BSD.var.dist -p /var > /dev/null
 
 # Make sure we have /var/log/utx.lastlogin and /var/log/utx.log files
 if [ ! -f /var/log/utx.lastlogin ]; then
@@ -14,6 +12,5 @@ if [ ! -f /var/log/utx.log ]; then
 	install -m644 /dev/null /var/log/utx.log
 fi
 
-mkdir -p /var/run/runit
-rm -rf /var/run/runit/*
+mkdir -p /var/run/runit/runsvdir
 install -m100 /dev/null /var/run/runit/stopit
