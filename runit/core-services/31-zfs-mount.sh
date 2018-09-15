61e0c4654f47877c65f18a2785b09749614f1331
Tobias Kortkamp <tobik@FreeBSD.org>
2018-09-15 14:23:11 +0200

    ZFS file systems -> ZFS datasets
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/31-zfs-mount.sh
+++ runit/core-services/31-zfs-mount.sh
@@ -2,15 +2,15 @@
 check_arch powerpc || return 0
 
 if [ -z "${JAILED}" ]; then
-	msg "Mounting ZFS file systems"
+	msg "Mounting ZFS datasets"
 	zfs mount -va || emergency_shell
 
-	msg "Sharing ZFS file systems"	
+	msg "Sharing ZFS datasets"	
 	zfs share -a || emergency_shell
 	if [ ! -r /etc/zfs/exports ]; then
 		touch /etc/zfs/exports
 	fi
 elif [ $(sysctl -n security.jail.mount_zfs_allowed) -eq 1 ]; then
-	msg "Mounting ZFS file systems"
+	msg "Mounting ZFS datasets"
 	zfs mount -a || emergency_shell
 fi
