8efe545f7cd4c8c9d856a5abe61f82a10f7ee3db
Tobias Kortkamp <tobik@FreeBSD.org>
2018-10-26 13:40:29 +0200

    Add a devfs.rules core service
    Should fix #2
    
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

---
+++ runit/core-services/41-devfs-rules.sh
@@ -0,0 +1,32 @@
+[ -n "$JAILED" ] && return 0
+
+awk 'BEGIN {
+	FS="="
+	ruleset_num = -1
+	ruleset_name = ""
+	print "set -eu"
+}
+
+/^#/ || /^[[:blank:]]*$/ {
+	next
+}
+
+/^\[.*=[0-9]*\]$/ {
+	ruleset_name = substr($1, 2, length($1) - 1)
+	ruleset_num = substr($2, 1, length($2) - 1)
+	rulesets[ruleset_name] = ruleset_num
+	printf "%s=%s\n", ruleset_name, ruleset_num
+	printf "devfs rule -s $%s delset\n", ruleset_name
+	next
+}
+
+ruleset_num != -1 {
+	printf "devfs rule -s $%s %s\n", ruleset_name, $0
+}
+
+END {
+	if (rulesets["runit_devfs_rules"] != "") {
+		print "devfs ruleset $runit_devfs_rules"
+		print "devfs rule applyset"
+	}
+}' /etc/defaults/devfs.rules /etc/devfs.rules | sh
