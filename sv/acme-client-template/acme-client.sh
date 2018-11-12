c7c22eb4aa7605f928e0e81419caef1e916d36e1
Tobias Kortkamp <tobik@FreeBSD.org>
2018-11-12 06:16:01 +0100

    acme-client-template: make less awk-ward
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- sv/acme-client-template/acme-client.sh
+++ sv/acme-client-template/acme-client.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 [ -r conf ] && . ./conf
-domain=$(echo "${PWD}" | awk -F@ '{ print $2 }')
-altnames=$(echo "${PWD}" | awk -F@ '{ for (i = 3; i <= NF; i++) { print $i }; }')
+# acme-client@<domain>[@<altname1>[@<alt2>[@<altN>]]]
+myifs="${IFS}"
+IFS="@"
+i=0
+for f in ${PWD}; do
+	case ${i} in
+	1) domain=${f} ;;
+	*) altnames="${altnames} ${f}" ;;
+	esac
+	i=$((i + 1))
+done
+IFS="${myifs}"
 : "${CHALLENGEDIR:=/usr/jails/http/usr/local/www/acme-client/${domain}}"
 : "${SSLDIR:=/usr/local/etc/ssl}"
 : "${ALTNAMES:=}"
