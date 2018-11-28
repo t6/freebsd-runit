e3167110d78fb7cf7e52a68a7afd5976aed6af75
Tobias Kortkamp <tobik@FreeBSD.org>
2018-11-28 15:45:16 +0100

    acme-client-template: Add support for Hiawatha
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- sv/acme-client-template/acme-client.sh
+++ sv/acme-client-template/acme-client.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
-[ -r conf ] && . ./conf
-# acme-client@<domain>[@<altname1>[@<alt2>[@<altN>]]]
-myifs="${IFS}"
-IFS="@"
-i=0
-for f in ${PWD}; do
-	case ${i} in
-	1) domain=${f} ;;
-	*) altnames="${altnames} ${f}" ;;
-	esac
-	i=$((i + 1))
-done
-IFS="${myifs}"
-: "${CHALLENGEDIR:=/usr/jails/http/usr/local/www/acme-client/${domain}}"
-: "${SSLDIR:=/usr/local/etc/ssl}"
-: "${ALTNAMES:=}"
+# shellcheck source=sv/acme-client-template/settings.sh
+. ./settings.sh
 install -d -o root -g wheel -m 755 "${SSLDIR}/${domain}"
-install -d -m700 -o root -g wheel "${SSLDIR}/${domain}/private"
+install -d -m 700 -o root -g wheel "${SSLDIR}/${domain}/private"
 install -d -o root -g www -m 770 "${CHALLENGEDIR}"
 # shellcheck disable=SC2086
 exec /usr/local/bin/acme-client -b \
---
+++ sv/acme-client-template/settings.sh
@@ -0,0 +1,16 @@
+[ -r conf ] && . ./conf
+# acme-client@<domain>[@<altname1>[@<alt2>[@<altN>]]]
+myifs="${IFS}"
+IFS="@"
+i=0
+for f in ${PWD}; do
+	case ${i} in
+	1) domain=${f} ;;
+	*) altnames="${altnames} ${f}" ;;
+	esac
+	i=$((i + 1))
+done
+IFS="${myifs}"
+: "${CHALLENGEDIR:=/usr/jails/http/usr/local/www/acme-client/${domain}}"
+: "${SSLDIR:=/usr/local/etc/ssl}"
+: "${ALTNAMES:=}"
