e9d2e3057bef049b0bd5189528458ca2a203e5a1
Tobias Kortkamp <tobik@FreeBSD.org>
2019-04-01 10:24:24 +0200

    Disable SC1004
    "This backslash+linefeed is literal. Break outside single quotes if you just want to break the line."
    
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- runit/core-services/99-ports-env.sh
+++ runit/core-services/99-ports-env.sh
@@ -4,6 +4,7 @@
 # things.  If you update your ports checkout or change something
 # in /etc/make.conf you might have to rerun this script.
 echo "=> Caching ports collection environment"
+# shellcheck disable=SC1004
 su -m nobody -c 'env MAKE=/usr/bin/make \
 	PORTSDIR="/usr/ports" \
 	SCRIPTSDIR="/usr/ports/Mk/Scripts" \
