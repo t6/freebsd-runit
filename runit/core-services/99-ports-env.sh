ab939fd4715fbe9bb897d380bb43ea118bf9187e
Tobias Kortkamp <tobik@FreeBSD.org>
2019-04-01 10:12:26 +0200

    Add core service to cache ports environment
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

---
+++ runit/core-services/99-ports-env.sh
@@ -0,0 +1,11 @@
+[ -r /usr/ports/Mk/Scripts/ports_env.sh ] || return 0
+
+# /var/run/runit/ports-env can be sourced to speed up ports related
+# things.  If you update your ports checkout or change something
+# in /etc/make.conf you might have to rerun this script.
+echo "=> Caching ports collection environment"
+su -m nobody -c 'env MAKE=/usr/bin/make \
+	PORTSDIR="/usr/ports" \
+	SCRIPTSDIR="/usr/ports/Mk/Scripts" \
+	/bin/sh "/usr/ports/Mk/Scripts/ports_env.sh"' \
+	>/var/run/runit/ports-env
